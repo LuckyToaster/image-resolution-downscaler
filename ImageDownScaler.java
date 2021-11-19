e46986b8eb0e825f6bdb554edaa7445bedcce1e7
LuckyToast <91315326+LuckyToaster@users.noreply.github.com>
2021-11-19 16:24:01 +0100

    Add files via upload

---
+++ ImageDownScaler.java
@@ -0,0 +1,63 @@
+package image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+import javax.imageio.ImageIO;
+//import java.util.Arrays;
+
+public class ImageDownScaler {
+	public static void main(String[] args) throws IOException {
+
+		// user input
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter image URL: ");
+		String url = in.nextLine();
+		System.out.print("Enter the new filename: ");
+		String fileName = in.nextLine();
+		System.out.print("By how much do you want to downscale? [1-100]: ");
+		double factor = in.nextInt();
+		in.close();
+			
+		// read the image
+		File file = new File(url);
+		BufferedImage image = ImageIO.read(file);
+			
+		// new width & height
+		int nw = (int) (image.getWidth() * (factor / 100));
+		int nh = (int) (image.getHeight() * (factor / 100));
+		int pixelStep = (int) (100 / factor);
+		
+
+		// pixel matrix with desired dimensions
+		int[][] pixels = new int[nh][nw];
+
+		// fill the pixel matrix with color values from the image
+		for (int row=0; row < pixels.length; row++) {
+			for (int col=0; col < pixels[row].length; col++) {
+				pixels[row][col] = image.getRGB((col * pixelStep), (row * pixelStep));
+			}
+		}
+
+		// instantiate the output image
+		BufferedImage output = new BufferedImage(nw, nh, BufferedImage.TYPE_INT_RGB);
+
+		// add pixels to the output image from the pixel matrix
+		for (int row=0; row < pixels.length; row++) {
+			for (int col=0; col < pixels[0].length; col++) {
+				int value = pixels[row][col];
+	               output.setRGB(col, row, value);
+			}
+		}
+			
+		// write the output image
+		String directory = Paths.get(url).getParent().toString();
+		File outputImage = new File(directory + "\\" + fileName);
+		ImageIO.write(output, "png", outputImage);
+		
+	}
+}
+
+
